e9e7b4700dda2b70f593f4e3549c8313dfcc1c68
Maxim Yefremov <maxYefr@gmail.com>
2017-08-07 17:40:04 +0300

    go fmt

--- apps/go-test2/main.go
+++ apps/go-test2/main.go
@@ -1,19 +1,20 @@
 package main
 
 import (
-	"github.com/ypapax/go-test2"
 	"flag"
-	"github.com/golang/glog"
 	"strings"
+
+	"github.com/golang/glog"
+	"github.com/ypapax/go-test2"
 )
 
-func main(){
+func main() {
 	var connStr, servePort, endpoints string
-	flag.StringVar(&connStr, "conn", "","mongo db connection string")
-	flag.StringVar(&servePort, "port", "8181","server binding port")
-	flag.StringVar(&endpoints, "endpoints", "current_speed,temperature","comma separated endpoints")
+	flag.StringVar(&connStr, "conn", "", "mongo db connection string")
+	flag.StringVar(&servePort, "port", "8181", "server binding port")
+	flag.StringVar(&endpoints, "endpoints", "current_speed,temperature", "comma separated endpoints")
 	flag.Parse()
-	if err := go_test2.Launch(connStr, servePort, strings.Split(endpoints,",")); err != nil {
+	if err := go_test2.Launch(connStr, servePort, strings.Split(endpoints, ",")); err != nil {
 		glog.Error(err)
 	}
 }
