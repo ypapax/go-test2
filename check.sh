fcbf77753f2b0dfe3f8b04e1e083050adedace94
Maxim Yefremov <maxYefr@gmail.com>
2017-08-07 14:49:30 +0300

    fixed check args and result cut

--- check.sh
+++ check.sh
@@ -10,11 +10,11 @@ for ep in temperature fake_ep current_speed ; do
     echo
     for act in  "/max" "/avg" "/fake" "/min" ""; do
         echo
-        for dates in "?start=01/01/2016" "" "?start=01/01/2016&stop=01/01/2017" "?stop=01/01/2006"; do
+        for dates in "?start=01/01/2016" "" "?start=01/01/2016&stop=05/12/2016" "?stop=01/01/2006"; do
                 url="$addr/test/api/v1/${ep}${act}${dates}"
                 cmd="curl -sS $url"
                 echo $cmd
-                $cmd | cut -c-100
+                $cmd | cut -c-200
 #                perf $url
                 echo
 #                curl -s -o /dev/null -w "%{http_code}" $url
